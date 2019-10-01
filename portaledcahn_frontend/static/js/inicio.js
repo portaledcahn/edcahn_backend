8b9067879a7d0bd3a2580770e386ac5b4717ea39
locazho <bryant_bmp@hotmail.com>
2019-10-01 10:26:23 -0600

    cambio en inicio

--- portaledcahn_frontend/static/js/inicio.js
+++ portaledcahn_frontend/static/js/inicio.js
@@ -4,18 +4,18 @@ CargarEstadisticas();
 $(function(){
     /*Anadir eventos a los elementos*/
     $('#campoBlancoBusquedaProcesosBoton').on('click',function(e){
-        window.location.href="/busqueda?term="+ encodeURIComponent($('#campoBlancoBusquedaProcesos').val()) +'&metodo=proceso';
+        window.location.href="/busqueda?term="+ encodeURIComponent($('#campoBlancoBusquedaProcesos').val()) +'&metodo='+ObtenerMetodo();
     });
     $('#campoBlancoBusquedaProcesos').on('keydown',function(e){
         teclaCodigo=e.keyCode ? e.keyCode : e.which;
         if(teclaCodigo=='13'){
-            window.location.href="/busqueda?term="+encodeURIComponent($('#campoBlancoBusquedaProcesos').val()) +'&metodo=proceso';
+            window.location.href="/busqueda?term="+encodeURIComponent($('#campoBlancoBusquedaProcesos').val()) +'&metodo='+ObtenerMetodo();
         }
     });
     /*Anadir Titles a los campos de busqueda*/
     AgregarToolTips();
     VerificarIntroduccion('INTROJS_INICIO',1);
-})
+});
 
 
 
@@ -49,4 +49,8 @@ function EfectoEstadisticas(){
           return value;
       }
       });
+}
+function ObtenerMetodo(){
+    return $('#metodoProceso')[0].checked?'proceso':$('#metodoContrato')[0].checked?'contrato':$('#metodoPago')[0].checked?'pago':'proceso';
+
 }
