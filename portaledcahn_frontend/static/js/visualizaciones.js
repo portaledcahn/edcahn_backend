9f2bd1da6c7bd6b1f485a8124f453196f98923c5
locazho <bryant_bmp@hotmail.com>
2020-01-10 15:21:09 -0600

    2019

--- portaledcahn_frontend/static/js/visualizaciones.js
+++ portaledcahn_frontend/static/js/visualizaciones.js
@@ -1,13 +1,13 @@
 function ObtenerEnlaceRed(tipo,direccion){
     switch(tipo){
         case 'whatsapp':
-        location.href='https://api.whatsapp.com/send?text='+encodeURIComponent(url+ direccion+'?año='+(new Date()).getFullYear());
+        location.href='https://api.whatsapp.com/send?text='+encodeURIComponent(url+ direccion+'?año='+2019/*(new Date()).getFullYear()*/);
         break;
         case 'facebook':
-        location.href='http://www.facebook.com/sharer.php?u='+encodeURIComponent(url+direccion+'?año='+(new Date()).getFullYear());
+        location.href='http://www.facebook.com/sharer.php?u='+encodeURIComponent(url+direccion+'?año='+2019/*(new Date()).getFullYear())*/;
         break;
         case 'twitter':
-        location.href='https://twitter.com/intent/tweet?text='+encodeURIComponent(url+direccion+'?año='+(new Date()).getFullYear());
+        location.href='https://twitter.com/intent/tweet?text='+encodeURIComponent(url+direccion+'?año='+2019/*(new Date()).getFullYear()*/);
         break;
         default:
         break;
@@ -15,5 +15,5 @@ function ObtenerEnlaceRed(tipo,direccion){
     
 }
 function AccederVisualizacion(direccion){
-    location.href=direccion+'?año='+(new Date()).getFullYear();
+    location.href=direccion+'?año='+2019/*(new Date()).getFullYear()*/;
 }
